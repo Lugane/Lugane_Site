44f9666a33be3eb240b847ad9c65c8013b0885da
Lugane <suportelugane@gmail.com>
2025-05-05 11:57:03 -0300

    subindo

--- document.js
+++ document.js
@@ -12,3 +12,29 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   });
+
+  document.querySelectorAll('.flecha_direita, .flecha_esquerda').forEach(flecha => {
+    flecha.addEventListener('click', () => {
+      const sliderId = flecha.dataset.slider;
+      const slider = document.getElementById(sliderId);
+      const scrollAmount = 320;
+  
+      if (flecha.classList.contains('flecha_direita')) {
+        slider.scrollLeft += scrollAmount;
+      } else {
+        slider.scrollLeft -= scrollAmount;
+      }
+    });
+  });
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const form = document.querySelector("form");
+  
+    form.addEventListener("submit", function () {
+      // Aguarda um pouquinho antes de limpar (para garantir que o envio ocorra)
+      setTimeout(() => {
+        form.reset(); // Limpa todos os campos do formulário
+      }, 100); // 100ms é tempo suficiente
+    });
+  });
+  
